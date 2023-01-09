7776e20540ccfe0386abcdf348f8b0937d7e8543
himanshu1214 <himanshu90gupta@outlook.com>
2023-01-09 00:56:31 -0600

    move routes to routes folde

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps')
 
 //Load env vars
 dotenv.config({ path: './config/config.env'});
 
+//Mount routes
 const app = express();
+app.use('/api/v1/bootcamps', bootcamps)
 
-app.get('/api/v1/bootcamps', (req, res) => {
-res.status(200).json({success: true, msg : 'Show all bootcamps'})
-})
-
-
-app.post('/api/v1/bootcamps/:id', (req, res) => {
-res.status(200).json({success: true, msg: `Added one bootcamp : ${req.params.id}`})
-})
-
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-res.status(200).json({success: true, msg: `Updated one bootcamp : ${req.params.id}`})
-})
-
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-res.status(200).json({success: true, msg: `Deleted one bootcamp : ${req.params.id}`})
-})
 const PORT = process.env.PORT || 5000;
 
 
