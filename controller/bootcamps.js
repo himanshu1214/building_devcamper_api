09c325aaa1f9628e45d334c54819fb3d8c585c55
himanshu1214 <himi.rockeveryone@gmail.com>
2023-01-14 20:45:33 -0600

    UPDATE and DELETE bootcamp

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -7,7 +7,7 @@ const Bootcamp = require('../models/bootcamps');
 exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamps = await Bootcamp.find();
-        res.status(200).json({ success: true, data: bootcamps});
+        res.status(200).json({ success: true, count: bootcamps.length,  data: bootcamps});
     }
         catch (err) {
             res.status(400).json({ success: false});
@@ -38,8 +38,22 @@ try {
 // @desc update Bootcamp by id
 // @route /api/v1/bootcamps/:id
 // @access Private
-exports.updateBootcamp = (req, res, next) => {
-res.status(200).json({ success: true, msg: `SHOW all bootcamps ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new : true, 
+            runValidators: true
+        });
+    
+        if (!bootcamp) {
+            return res.status(400).json({success: false});
+        }
+        res.status(200).json({ success: true, data: bootcamp})
+    }
+        catch (err) {
+            res.status(400).json({ success: false });
+    
+        }
 };
 
 
@@ -64,6 +78,17 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc delete Bootcamp by id
 // @route /api/v1/bootcamps/:id
 // @access Private
-exports.deleteBootcamp = (req, res, next) => {
-res.status(200).json({ success: true, msg: `SHOW all bootcamps ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    
+        if (!bootcamp) {
+            return res.status(400).json({success: false});
+        }
+        res.status(200).json({ success: true, data: {}})
+    }
+        catch (err) {
+            res.status(400).json({ success: false });
+    
+        }
 };
