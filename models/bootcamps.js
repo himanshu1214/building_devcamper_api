64eceea3e90925613cc0fd4850471bc854abfb56
himanshu1214 <himi.rockeveryone@gmail.com>
2023-01-15 02:45:32 -0600

    add SLUGIFY

--- models/bootcamps.js
+++ models/bootcamps.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 const BootcampSchema = new mongoose.Schema({
     name: { 
         type: String,
@@ -90,6 +92,13 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
         }
+});
+
+
+BootcampSchema.pre('save', function(next) {
+    console.log('Slugify run', this.name);
+    this.slug = slugify(this.name, { lower: true });
+    next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
