07987971e0e09a496e9fba64b804c712189f4e21
himanshu1214 <himi.rockeveryone@gmail.com>
2023-02-05 01:57:29 -0600

    add course data model and feed into MongoDB

---
+++ models/courses.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const course_schema = new mongoose.Schema({
+    title: {
+        type: String, 
+        trim: true,
+        required: [true,'Please add a course']
+    }, 
+
+    description: {
+        type: String,
+        required: [true, 'Please add description']
+    }, 
+
+    weeks: {
+        type: String,
+        required: [true, 'Please add number of weeks']
+    },
+
+    tuition: {
+        type: String,
+        required: [true, 'Please add tuition cost']
+    },
+
+    minimum_skill: {
+        type: String,
+        required: [true, 'Please add minimum skill'],
+        enum: ['beginner', 'intermediate', 'Master']
+    }, 
+    scholarships_available: {
+        type: String, 
+        required: [true, 'Please add scholarship availability']
+    },
+    create_at: {
+        type: Date,
+        default: Date.now
+    },
+    bootcamp: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Bootcamp',
+        required: true
+    }
+
+});
+
+module.exports = mongoose.model('Course', course_schema)
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // load models
 const Bootcamp = require('./models/bootcamps');
+const Course = require('./models/courses');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,9 +21,14 @@ const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
   );
 
+  // Add course to BD
+const courses = JSON.parse(
+fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 const importData = async() => {
 try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Create data in Db'.green.inverse);
     process.exit();
 } catch (err) {
@@ -34,6 +40,7 @@ try {
 const deleteData = async() => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Delete data in Db'.red.inverse);
         process.exit();
     } catch (err) {
