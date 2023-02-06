fe74dd1996e8160cbf57022c88c4770afb00777b
himanshu1214 <himi.rockeveryone@gmail.com>
2023-02-06 01:14:44 -0600

    GET single course and ADD SINGLE COURSE inside an existing bootcamp

--- controller/courses.js
+++ controller/courses.js
@@ -29,3 +29,48 @@ exports.getCourses = AsyncHandler(async (req, res, next) => {
         data: courses
     });
 });
+
+// @desc get one Course
+// @route /api/v1/course/:id
+// @access Public
+exports.getCourse = AsyncHandler(async (req, res, next) => {
+    // adding the bootcamp using populate for every course
+    const course = await Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    });
+    if (!course) { 
+        next(new ErrorResponse(`No course available with the id : ${req.param.id}`, 404))
+    };
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
+
+
+// @desc create Course for bootcamp in a particular BootcampId
+// @route /api/v1/bootcamps/:id/courses
+// @access Private
+exports.createCourse = AsyncHandler( async (req, res, next) => {
+
+    // error handling
+    try {
+        req.body.bootcamp = req.params.bootcampId;
+
+        const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+        console.log(bootcamp)
+        if (!bootcamp) {
+            return   next( new ErrorResponse(`the bootcamp doesnot exist with the id : ${req.params.bootcampId}`) ); 
+        };
+
+        const course = await Course.create(req.body);  //mongoose method returns a Promise
+
+        res.status(200).json({
+            success: true,
+            data: course
+        });
+    } catch (err) {
+        next(err);
+        }
+    }) ;
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
-const { getCourses } = require('../controller/courses');
+const { getCourses, getCourse, createCourse } = require('../controller/courses');
 
 const router = express.Router( {mergeParams: true} );
 
-router.route('/').get(getCourses);
+// course is created using /bootcamps/:bootcampId/courses endpoint
+// course router is used inside the bootcamp router 
+router.route('/').get(getCourses).post(createCourse);
+
+router.route('/:id').get(getCourse);
+
 
 module.exports = router;
