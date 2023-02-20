e53ee7f4dd5cb22d68415ea83c9c4ca0089a8687
himanshu1214 <himanshu90gupta@outlook.com>
2023-02-19 23:57:01 -0600

    add Method to create User in database

--- controller/auth.js
+++ controller/auth.js
@@ -2,14 +2,26 @@ const Bootcamp = require('../models/bootcamps');
 const Course = require('../models/courses');
 const ErrorResponse = require('../utils/errorResponse');
 const AsyncHandler = require('../middleware/async');
+const User = require('../models/users');
 
 // @desc Register User
 // @route /api/v1/auth/register
 // @access Public
 
 exports.register = AsyncHandler(async (req, res, next) => {
-  console.log(req);
+    const { name, email, password, role } = req.body;
+
+    // Create User in Database for Table User
+    const user  = await User.create(
+        {
+            name, 
+            email, 
+            password, 
+            role
+        }
+    )
   res.status(200).json({
     success: true,
+    user_data: user
   });
 });
--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcryptjs');
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -30,3 +32,11 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
